74c5292149e1638f4d5dd1be386ea2c94c6b354e
George <george.xenodohov@yandex.ru>
2024-02-01 22:27:14 +0300

    l

---
+++ Tracker/CategoryScreen/CategoryScreenViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryScreenViewModel.swift
+//  Tracker
+//
+//  Created by Георгий Ксенодохов on 30.01.2024.
+//
+
+import Foundation
+
+class CategoryScreenViewModel {
+    
+    var onChange: (() -> Void)?
+    
+    
+     private(set) var dataCategory: [String] = [] {
+        didSet {
+            onChange?()
+        }
+    }
+    func returnCategory(){
+        dataCategory = TrackerCategoryStore.shared.returnNamesOfCategory()
+    }
+    var numberOfCategory : Int {
+        dataCategory.count
+    }
+    
+}
